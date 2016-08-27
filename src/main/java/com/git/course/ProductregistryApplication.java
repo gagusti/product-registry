00acc33570e2bb4d7ddd3a99710c6b411cd76362
Guillermo Agusti <guillermo.agusti@hpe.com>
2016-08-27 15:38:59 -0300

    [US01] Create Product Repository Project

---
+++ src/main/java/com/git/course/ProductregistryApplication.java
@@ -0,0 +1,12 @@
+package com.git.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductregistryApplication.class, args);
+	}
+}
